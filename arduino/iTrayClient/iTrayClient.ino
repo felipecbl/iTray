f30fe394c0d9af6f08f8a324b268ff9b0e55a035
felipecbl <macuser@myMac-2.local>
2015-06-30 00:41:08 -0700

    Serial to 115200
    Changing Delay position so esp has more time to load content
    (WORKING!!!!!!)
    
    Getting values from JSON

--- arduino/iTrayClient/iTrayClient.ino
+++ arduino/iTrayClient/iTrayClient.ino
@@ -8,23 +8,33 @@
 
 #include <ESP8266WiFi.h>
 
+const String current_angle = "\"current_angle\":\"";
+const String tray_connected = "\"tray_connected\":\"";
+const String user_connected = "\"user_connected\":\"";
+const String last_angle = "\"last_angle\":\"";
+
 const char* ssid     = "White Castle";
 const char* password = "castel0branc0";
 
 //const char* host = "data.sparkfun.com";
-const char* host = "www.p3studio.ca";
+const char* host = "www.itray.website";
 
-void getWiFi(){ 
+void getWiFi() {
   while (WiFi.status() != WL_CONNECTED) {
     digitalWrite(2, HIGH);
     delay(500);
     Serial.print(".");
     digitalWrite(2, LOW);
   }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(2, OUTPUT);
   delay(10);
 
@@ -34,26 +44,18 @@ void setup() {
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
-  
-  WiFi.begin(ssid, password);
 
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
   getWiFi();
 
-  Serial.println("");
-  Serial.println("WiFi connected");  
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
 }
 
-//int value = 0;
-
 void loop() {
-
   getWiFi();
-  delay(2000);
-//  ++value;
+//  delay(2000);
   digitalWrite(2, HIGH);
-  
+
   // Use WiFiClient class to create TCP connections
   WiFiClient client;
   const int httpPort = 80;
@@ -69,45 +71,55 @@ void loop() {
     Serial.print("Connected to: ");
     Serial.println(host);
 
-    String postString = " /itray/getWheel.php";
-//    String postString = " /home/content/36/5998036/html/itray/getWheel.php";
-//    postString += "?angle=";
-//    postString += trayAngle;
-//    postString += "&json" ;
+    String postString = " /getWheel.php?json";
+    //    postString += "?angle=";
+    //    postString += trayAngle;
+    //    postString += "&json" ;
 
-
-    client.print(String("GET ") + postString + "HTTP/1.1\n"); 
-    client.print("Host: www.p3studio.ca\n"); 
-    client.print("Connection: close\n"); 
-    client.print("Content-Type: text/html\n"); 
-    client.print("Content-Length: "); 
-    client.print(postString.length());  
-    client.print("\n\n"); 
-    client.print(postString);   
-
-//client.print("Accept:  text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
-//client.print("Accept-Encoding: gzip, deflate");
-//client.print("Accept-Language: en-US,en;q=0.5");
-//client.print("Cache-Control: max-age=0");
-//client.print("Connection:  keep-alive");
-//client.print("Host:  www.p3studio.ca");
-
-    
     Serial.print("Requesting URL: ");
     Serial.println(postString);
-    
-    // This will send the request to the server
-//    client.print(String("GET ") + postString + " HTTP/1.1\r\nHost: " +host + "\r\n" + "Connection: close\r\n\r\n");
-    
-    // Read all the lines of the reply from server and print them to Serial
-  //  while(client.available()){
-      String line = client.readStringUntil('\r');
-      Serial.println(line);
 
-  //  }
+    // This will send the request to the server
+    client.print(String("GET ") + postString + " HTTP/1.1\r\n" +
+                 "Host: " + host + "\r\n" +
+                 "Connection: close\r\n\r\n");
     
+    delay(500);
+
+    // Read all the lines of the reply from server and print them to Serial
+    while (client.available()) {
+      String line = client.readStringUntil('\r');
+      Serial.print(line);
+
+      if (line.indexOf("\"success\":true") != -1){
+        Serial.println("Success!!");
+
+        Serial.print("Current angle: ");        
+        Serial.println(getValue(line, current_angle, "\""));
+
+        Serial.print("Current Tray Connected: ");        
+        Serial.println(getValue(line, tray_connected, "\""));
+
+        Serial.print("Current User Connected: ");        
+        Serial.println(getValue(line, user_connected, "\""));
+
+        Serial.print("Current last angle: ");        
+        Serial.println(getValue(line, last_angle, "\""));        
+      }      
+    }
+
     Serial.println();
     Serial.println("closing connection");
   }
+}
+
+String getValue(String webString, String startString, String endString) {
+  if (webString.indexOf(startString) != -1){
+      int sStart = webString.indexOf(startString) + startString.length();
+      int sEnd = webString.indexOf(endString, sStart);
+      
+      return  webString.substring(sStart, sEnd);
+  }
+  return "";
 }
 
